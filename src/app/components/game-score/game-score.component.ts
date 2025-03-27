04d899609826bc6f6775ed28fc1a016a385ad1f7
Vinicius Rodrigues <vinicius.worms@hotmail.com>
2025-03-28 01:17:54 +0800

    Added game-score component

---
+++ src/app/components/game-score/game-score.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-score',
+  templateUrl: './game-score.component.html',
+  styleUrls: ['./game-score.component.css']
+})
+export class GameScoreComponent {
+
+}
