4fc1c46869ac12c9e8731c6c31444a53a4123081
Vinicius Rodrigues <vinicius.worms@hotmail.com>
2025-03-28 11:37:35 +0800

    Game score html and ts settled

--- src/app/components/game-score/game-score.component.spec.ts
+++ src/app/components/game-score/game-score.component.spec.ts
@@ -1,21 +1,37 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { GameDetailsComponent } from '../game-details/game-details.component';
 
-import { GameScoreComponent } from './game-score.component';
+@Component({
+  selector: 'app-game-score',
+  templateUrl: './game-score.component.html',
+  styleUrls: ['./game-score.component.scss']
 
-describe('GameScoreComponent', () => {
-  let component: GameScoreComponent;
-  let fixture: ComponentFixture<GameScoreComponent>;
+})
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [GameScoreComponent]
-    });
-    fixture = TestBed.createComponent(GameScoreComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+export class GameScore {
+  // hover = false;
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  team1 = {
+    name: 'Corinthians', 
+    score: 1, 
+    logo: 'https://img.icons8.com/color/48/corinthians.png';
+  }
+
+  team2 = {
+    name: 'Chelsea', 
+    score: 0, 
+    logo: 'https://img.icons8.com/color/48/chelsea-fc.png';
+  }
+
+  constructor(public dialog: MatDialog) {}
+
+  teamDescription(){
+    this.dialog.open(GameDetailsComponent, {
+      width: '480px',
+      data: {test: 'test'}
+    })
+    console.log('Team description!');
+  }
+
+}
